202b4b82437d442b9591d434ab2e9dc558e13cf7
Dimitris Zervas <dzervas@dzervas.gr>
2022-08-06 19:22:23 +0300

    Add all logs in vpn gateway

--- tf/vpn.tf
+++ tf/vpn.tf
@@ -115,29 +115,21 @@ resource "azurerm_virtual_network_gateway" "vpn_remote" {
   tags = var.tags
 }
 
-resource "azurerm_monitor_diagnostic_setting" "vpn_remote_gateway" {
-  name                       = "clouderp-vpn-remote-gateway-analytics"
+data "azurerm_monitor_diagnostic_categories" "vpn_remote" {
+  resource_id = azurerm_virtual_network_gateway.vpn_remote.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "vpn_remote" {
+  name                       = "clouderp-vpn-remote-analytics"
   target_resource_id         = azurerm_virtual_network_gateway.vpn_remote.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
 
-  log {
-    category = "GatewayDiagnosticLog"
-  }
 
-  log {
-    category = "TunnelDiagnosticLog"
-  }
-
-  log {
-    category = "RouteDiagnosticLog"
-  }
-
-  log {
-    category = "IKEDiagnosticLog"
-  }
-
-  log {
-    category = "P2SDiagnosticLog"
+  dynamic "log" {
+    for_each = data.azurerm_monitor_diagnostic_categories.vpn_remote.logs
+    content {
+      category = log
+    }
   }
 
   metric {
